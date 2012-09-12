dfcbeef567abdd07f4b4b9d9cfe138458f752cf6
Rafael Brigagão Paulino <rafa_corre@MacBook-Pro-de-Rafael.local>
2012-09-12 11:23:13 -0300

    Initial Commit

---
+++ SensorProximidadeTests/SensorProximidadeTests.h
@@ -0,0 +1,13 @@
+//
+//  SensorProximidadeTests.h
+//  SensorProximidadeTests
+//
+//  Created by Rafael Brigagão Paulino on 12/09/12.
+//  Copyright (c) 2012 rafapaulino.com. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface SensorProximidadeTests : SenTestCase
+
+@end
---
+++ SensorProximidadeTests/SensorProximidadeTests.m
@@ -0,0 +1,32 @@
+//
+//  SensorProximidadeTests.m
+//  SensorProximidadeTests
+//
+//  Created by Rafael Brigagão Paulino on 12/09/12.
+//  Copyright (c) 2012 rafapaulino.com. All rights reserved.
+//
+
+#import "SensorProximidadeTests.h"
+
+@implementation SensorProximidadeTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in SensorProximidadeTests");
+}
+
+@end
